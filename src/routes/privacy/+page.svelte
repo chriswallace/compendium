d18fc18f16773cf1b9ace1075b77c9ba0b1ee6a9
Chris Wallace <chriswallace7@protonmail.com>
2024-01-26 08:42:05 -0600

    privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,104 @@
+<header>
+  <h1>Compendium Privacy Policy</h1>
+  <p>Last Updated: <time datetime="2024-01-26">January 26th, 2024</time></p>
+</header>
+
+<section>
+  <h2>1. Information Collection</h2>
+  <p>
+    We collect information that identifies, relates to, describes, or is capable
+    of being associated with you (“Personal Information”) when you:
+  </p>
+  <ul>
+    <li>Use our application</li>
+    <li>Register an account</li>
+    <li>Interact with our services</li>
+  </ul>
+  <p>Types of data collected may include:</p>
+  <ul>
+    <li>
+      Personal identifiers like your name, email address, and phone number.
+    </li>
+    <li>
+      Internet or other electronic network activity information, including, but
+      not limited to, browsing history, search history, and information
+      regarding your interaction with the application.
+    </li>
+  </ul>
+</section>
+
+<section>
+  <h2>2. Use of Your Information</h2>
+  <p>
+    We may use the information we collect about you for various purposes,
+    including to:
+  </p>
+  <ul>
+    <li>Provide, maintain, and improve our services.</li>
+    <li>Personalize your experience.</li>
+    <li>
+      Understand and analyze how you use our app for improving functionality.
+    </li>
+    <li>Develop new products, services, and features.</li>
+    <li>
+      Communicate with you, either directly or through one of our partners.
+    </li>
+  </ul>
+</section>
+
+<section>
+  <h2>3. Sharing Your Information</h2>
+  <p>
+    We do not sell your personal information. However, we may share information
+    with our trusted partners for the following purposes:
+  </p>
+  <ul>
+    <li>Facilitate our service.</li>
+    <li>Perform service-related services.</li>
+    <li>Assist us in analyzing how our service is used.</li>
+  </ul>
+</section>
+
+<section>
+  <h2>4. Data Security</h2>
+  <p>
+    We implement a variety of security measures to maintain the safety of your
+    personal information. However, please remember that no method of
+    transmission over the internet, or method of electronic storage is 100%
+    secure.
+  </p>
+</section>
+
+<section>
+  <h2>5. Your Privacy Rights</h2>
+  <p>
+    Depending on your region, you may have rights regarding your personal data,
+    such as:
+  </p>
+  <ul>
+    <li>
+      The right to access, update or delete the information we have on you.
+    </li>
+    <li>The right of rectification.</li>
+    <li>The right to object.</li>
+    <li>The right of restriction.</li>
+    <li>The right to data portability.</li>
+    <li>The right to withdraw consent.</li>
+  </ul>
+</section>
+
+<section>
+  <h2>6. Changes to This Privacy Policy</h2>
+  <p>
+    We may update our Privacy Policy from time to time. We will notify you of
+    any changes by posting the new Privacy Policy on this page.
+  </p>
+</section>
+
+<footer>
+  <h2>7. Contact Us</h2>
+  <p>
+    If you have any questions about this Privacy Policy, please contact us at
+    <a href="mailto:support@cpdm.app">support@cpdm.app</a>.
+  </p>
+</footer>
